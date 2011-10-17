bfb2fd699f081df5b5867b5ec8e216b9a9a9917a
Saurabh Jain <saurabhjain.net@gmail.com>
2011-10-16 21:35:07 -0700

    Final version

--- src/com/example/bluetooth/BluetoothActivity.java
+++ src/com/example/bluetooth/BluetoothActivity.java
@@ -1,5 +1,9 @@
 package com.example.bluetooth;
 
+/**
+ * @author Saurabh
+ * This tutorial list you any of your paired bluetooth devices.
+ */
 import java.util.Set;
 
 import android.app.Activity;
@@ -11,35 +15,48 @@ import android.widget.TextView;
 public class BluetoothActivity extends Activity {
 
 	TextView out;
-	
+	int currentstate;
+
 	/** Called when the activity is first created. */
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        out = (TextView)findViewById(R.id.out);
-        
-        // Getting the Bluetooth adaptor
-        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-        out.append("\n Adapter:" + adapter);
-        
-        // Checking for Bluetooth support
-        if(adapter == null) {
-        	out.append("\n Bluetooth not supported, Aborting !");
-        	return;
-        }
-        
-        // Starting the device discovery
-        out.append("\n \n Searching for bluetooth devices nearby...");
-        adapter.startDiscovery();
-        out.append("\n \n Finished searching for bluetooth devices...");
-        
-        //Listing paired devices
-        out.append("\n \n Listing the paired devices:");
-        Set<BluetoothDevice> devices = adapter.getBondedDevices();
-        for(BluetoothDevice device: devices) {
-        	out.append("\n \n Found device: " + device);
-        }
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		out = (TextView) findViewById(R.id.out);
+
+		// Getting the Bluetooth adaptor
+		BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+		// out.append("\n Adapter:" + adapter);
+
+		// Checking for Bluetooth support
+		if (adapter == null) {
+			out.append("\n Bluetooth not supported, Aborting !");
+			return;
+		}
+
+		currentstate = adapter.getState();
+		if (currentstate == 12) {
+			out.append("\n Bluetooth is on...");
+		} else if (currentstate == 10) {
+			out.append("\n Bluetooth is not turned on, turn it on and restart the app.");
+			return;
+		} else {
+			out.append("\n Error occured, restart app after turning on bluetooth.");
+			return;
+		}
+
+		// Starting the device discovery
+		out.append("\n \n Searching for bluetooth devices nearby "
+				+ adapter.getName() + "...");
+		adapter.startDiscovery();
+		out.append("\n \n Finished searching for bluetooth devices...");
+
+		// Listing paired devices
+		out.append("\n \n Listing the paired devices with " + adapter.getName() + ":");
+		Set<BluetoothDevice> devices = adapter.getBondedDevices();
+		for (BluetoothDevice device : devices) {
+			out.append("\n \n Found device: " + device);
+		}
+	}
 }
